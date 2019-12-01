636592096cab315ee63ae9b86cd2fa89fd2cc8e5
Dombrovskyi <dombrovskiy2098@gmail.com>
2019-12-01 19:53:31 +0200

    Updates

---
+++ precache-manifest.1a88c2b8254c3391b0c7f54d23f62645.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "34206bdeae1cba8ac6bce83716f3cd40",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "cba6ab96633e2e3d8130",
+    "url": "/monsters-rolodex/static/css/main.cffa58ed.chunk.css"
+  },
+  {
+    "revision": "3a057e02db196afa0ca3",
+    "url": "/monsters-rolodex/static/js/2.f833121f.chunk.js"
+  },
+  {
+    "revision": "cba6ab96633e2e3d8130",
+    "url": "/monsters-rolodex/static/js/main.b0e3f75a.chunk.js"
+  },
+  {
+    "revision": "2dc9b40a277f725e4d87",
+    "url": "/monsters-rolodex/static/js/runtime-main.b05ca383.js"
+  }
+]);
